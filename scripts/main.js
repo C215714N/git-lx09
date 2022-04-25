a18caade952c2efa55dafdc6395c24cffeef531d
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-04-25 09:51:40 -0300

    Estilos: Estructura General
    Estilos Secciones de contenido
    
    Estilos: Contenido Principal
    
    Estructura Apuntadores y Contenedor mapa

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,19 @@
     const d = document
     const menuBtn = d.querySelector('nav .btn')
     const menuLst = d.querySelector('nav .menu')
+    const mapFrm = d.querySelector('#footer .mapaContacto')
+    const mapBtn = d.querySelector('#footer .mapButton')
 
 // Navegacion
     menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = "&times;" : menuBtn.innerHTML = "&equiv;"
+
+// Mapa de Contacto
+    mapBtn.addEventListener('click', () => toggleClass('active', 'icon-min', 'icon-max') )
+
+    function toggleClass(className, a, b){
+        if(mapFrm.classList.toggle(className)){
+            mapBtn.classList.replace(b,a)
+        } else {
+            mapBtn.classList.replace(a,b)
+        }
+    }
